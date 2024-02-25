2acf3d044d4593802ffbceb2bab2087ac0678a0a
Aman <jhonman02@gmail.com>
2024-02-25 05:46:28 +0530

    fixed the swagger host after deployment

--- server/swagger.js
+++ server/swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: 'localhost:3000'
+  host: 'https://koinx-laft.onrender.com'
 };
 
 const outputFile = './swagger-output.json';
